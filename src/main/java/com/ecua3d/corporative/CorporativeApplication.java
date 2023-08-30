a20063f49ed3f57b8764a5daaac540df0fc7b2c8
mnchicaiza <noemi-2305@hotmail.com>
2023-08-29 20:31:59 -0500

    first commit

---
+++ src/main/java/com/ecua3d/corporative/CorporativeApplication.java
@@ -0,0 +1,13 @@
+package com.ecua3d.corporative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecua3d/corporative/CorporativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecua3d.corporative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
