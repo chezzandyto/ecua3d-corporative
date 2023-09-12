4e95f83fd28e08fcc869382495c64ff123c3ebb6
mnchicaiza <noemi-2305@hotmail.com>
2023-09-11 20:59:06 -0500

    configuracion security

--- src/main/java/com/ecua3d/corporative/security/TokenConverterProperties.java
+++ src/main/java/com/ecua3d/corporative/security/TokenConverterProperties.java
@@ -1,2 +1,29 @@
-package com.ecua3d.corporative.security;public class TokenConverterProperties {
+package com.ecua3d.corporative.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
+
+@Configuration
+@ConfigurationProperties(prefix = "token.converter")
+public class TokenConverterProperties {
+    private String resourceId;
+    private String principalAttribute;
+
+    public String getResourceId() {
+        return resourceId;
+    }
+
+    public Optional<String> getPrincipalAttribute() {
+        return Optional.ofNullable(principalAttribute);
+    }
+
+    public void setResourceId(String resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public void setPrincipalAttribute(String principalAttribute) {
+        this.principalAttribute = principalAttribute;
+    }
 }
